7f194fab5482d0fcacc30cb0fff00ebc24887c4b
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-10-25 13:28:27 +0300

    Ctreate DbConnectionExtension

--- backend/RealTimeChat.Infrastructure/ConfigureServices.cs
+++ backend/RealTimeChat.Infrastructure/ConfigureServices.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RealTimeChat.Application.Interfaces;
+using RealTimeChat.Infrastructure.Extensions;
 using RealTimeChat.Infrastructure.Persistence;
 using RealTimeChat.Infrastructure.Repositories;
 
@@ -11,18 +11,7 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var localhost = "localhost";
-        var host = configuration["CHAT_DB_HOST"];
-        var port = configuration["CHAT_DB_PORT"];
-        var database = configuration["CHAT_DB_NAME"];
-        var username = configuration["CHAT_DB_USER"];
-        var password = configuration["CHAT_DB_PASSWORD"];
-
-        // For local development, override the host to localhost
-        var connectionString = $"Server={localhost},{port};Database={database};User Id={username};Password={password};TrustServerCertificate=True;";
-
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
+        services.AddSqlServerConnection(configuration);
 
         services.AddScoped<IApplicationDbContext>(provider =>
            provider.GetRequiredService<ApplicationDbContext>());
---
+++ backend/RealTimeChat.Infrastructure/Extensions/DbConnectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RealTimeChat.Infrastructure.Persistence;
+
+namespace RealTimeChat.Infrastructure.Extensions;
+
+public static class DbConnectionExtension
+{
+    public static IServiceCollection AddSqlServerConnection(this IServiceCollection services, IConfiguration configuration)
+    {
+        var localhost = "localhost";
+        var host = configuration["CHAT_DB_HOST"];
+        var port = configuration["CHAT_DB_PORT"];
+        var database = configuration["CHAT_DB_NAME"];
+        var username = configuration["CHAT_DB_USER"];
+        var password = configuration["CHAT_DB_PASSWORD"];
+
+        // For local development, override the host to localhost
+        var connectionString = $"Server={localhost},{port};Database={database};User Id={username};Password={password};TrustServerCertificate=True;";
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        return services;
+    }
+}
